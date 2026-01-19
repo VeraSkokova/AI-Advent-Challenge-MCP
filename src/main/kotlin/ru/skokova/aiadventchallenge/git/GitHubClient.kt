d5ee2ebf14dc9083ad559416352d4640508f2dbb
Vera Skokova <miss.smiley17@gmail.com>
2026-01-19 13:27:06 +0700

    Fix compilation error: Add missing import io.ktor.client.statement.*

--- src/main/kotlin/ru/skokova/aiadventchallenge/git/GitHubClient.kt
+++ src/main/kotlin/ru/skokova/aiadventchallenge/git/GitHubClient.kt
@@ -7,6 +7,7 @@ import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.* 
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.Serializable
